971e9ea4d2e2639ef283b7719eb123758a1e6a2c
KenSteelyHunter <31893777+antonusachenko@users.noreply.github.com>
2023-06-20 20:49:01 +0700

    try № 3 to make normal state machine for agent

--- Assets/Scripts/PlayerPersonController.cs
+++ Assets/Scripts/PlayerPersonController.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 using UnityEngine.AI;
 using UnityStandardAssets.Characters.ThirdPerson;
+using static UnityEngine.GraphicsBuffer;
 
 public class PlayerController : MonoBehaviour
 {
+    public enum PlayerState { ANALOG_RUN, NM_RUN, WAITING4PATH, NMRUN_REACHED, DEATH };
+    public PlayerState state;
 
     [SerializeField] private Camera _cam;
 
@@ -13,18 +16,25 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private Vector3 _target;
 
+    [SerializeField] private float _stopTime = 2f;
+
     public Vector3 characterDirection;
 
+    private Vector3 _navmeshTarget;
     private Vector3 _analogTarget;
+
+    private bool _isTargetPointReached;
 
     private bool _analogCharacterControlEnabled;
 
     private bool _agentDestinationCorrect;
 
+    private float _timerToSwitch4AnalogRun;
+
 
     private void Awake()
     {
-        EnableAnalogCharacterControl();
+        _agent.enabled = false;
         characterDirection = new Vector3(0, 0, 5f);
     }
 
@@ -35,56 +45,121 @@ public class PlayerController : MonoBehaviour
 
         //Setup
         _agent.updateRotation = false;
+        state = PlayerState.ANALOG_RUN;
     }
 
     void Update()
     {
-        if (_analogCharacterControlEnabled)
+        switch (state)
         {
-            _character.Move(new Vector3(0f, 0f, 1f), false, false);
+            case PlayerState.ANALOG_RUN:
+                //code
+                Debug.Log($"PLAYER: ANALOG_RUN ");
+                _agent.velocity = Vector3.zero;
+                _agent.enabled = false;
+                _character.Move(characterDirection, false, false);
+
+                break;
+
+            case PlayerState.NM_RUN:
+                //code
+                //Debug.Log($"PLAYER: _agent.remainingDistance {_agent.remainingDistance} ");
+                Debug.Log($"PLAYER: NM_RUN ");
+
+                // click control
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+                    RaycastHit hit;
+
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        _agent.SetDestination(hit.point);
+                    }
+                }
+
+                // NavMesh
+                if (_agent.remainingDistance > _agent.stoppingDistance)
+                {
+                    _character.Move(_agent.desiredVelocity, false, false);
+                }
+                else
+                {
+                    _character.Move(Vector3.zero, false, false);
+                    //_character.Move(_agent.desiredVelocity, false, false);
+                    //_agent.enabled = false; //agent needed time to stop
+                    if (_agent.velocity.magnitude < 0.01f)
+                        _agent.isStopped = true;
+                        state = PlayerState.NMRUN_REACHED;
+                }
+
+                break;
+
+            case PlayerState.WAITING4PATH:
+                //code
+                Debug.Log($"PLAYER: WAITING4PATH ");
+                _character.Move(Vector3.zero, false, false);
+                if (_agent.isActiveAndEnabled && _agentDestinationCorrect && _agent.remainingDistance != null)
+                {
+                    state = PlayerState.NM_RUN;
+                }
+
+                break;
+
+            case PlayerState.NMRUN_REACHED:
+                //code
+                //_character.Move(Vector3.zero, false, false);
+
+                Debug.Log($"PLAYER: NMRUN_REACHED ");
+                characterDirection = (_analogTarget - transform.position).normalized;
+
+                //if (_timerToSwitch4AnalogRun > 0)
+                //{
+                //    _timerToSwitch4AnalogRun -= Time.deltaTime;
+                //}
+                //else
+                //{
+                //    state = PlayerState.ANALOG_RUN;
+                //}
+
+                if(_agent.velocity.magnitude < 0.01f)
+                    state = PlayerState.ANALOG_RUN;
+
+                break;
+
+            case PlayerState.DEATH:
+                //code
+                break;
+
+            default:
+                //code
+                _character.Move(Vector3.zero, false, false);
+                Debug.LogWarning($"PLAYER: WAITING FOR PATH ");
+                break;
         }
 
-        if (!_analogCharacterControlEnabled && Input.GetMouseButtonDown(0))
-        {
-            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit))
-            {
-                _agent.SetDestination(hit.point);
-            }
-        }
 
-        if (!_analogCharacterControlEnabled && _agent.remainingDistance > _agent.stoppingDistance)
-        {
-            _character.Move(_agent.desiredVelocity, false, false);
-        }
-        else
-        {
-            _character.Move(Vector3.zero, false, false);
-        }
+        
     }
 
     private void GM_OnGetNewTarget(object sender, GameManager.OnGetNewTargetEventArgs e)
     {
-        SetAgentDestination(e.chunkTarget);
+        _navmeshTarget = e.chunkTarget;
         _analogTarget = e.analogTarget;
-
-        //Debug.Log($"PLAYER: Aprove chunk target point {e.chunkTarget} and next target point {e.analogTarget}");
-    }
-
-    private void SetAgentDestination(Vector3 target)
-    {
-        _agentDestinationCorrect = _agent.SetDestination(target);
+        _agent.enabled = true;
+        _agentDestinationCorrect = _agent.SetDestination(_navmeshTarget);
         if (_agentDestinationCorrect)
         {
-            EnableNavMeshCharacterControl();
-            Debug.Log($"PLAYER: Aprove chunk target point {target} ");
+            state = PlayerState.WAITING4PATH;
+            Debug.Log($"PLAYER: Aprove chunk target point {_navmeshTarget} ");
         }
         else
         {
-            //SetAgentDestination(target);
+            Debug.LogError($"PLAYER: NAVMESH TARGET SET FAILED");
         }
+
+        //Debug.Log($"PLAYER: Aprove chunk target point {e.chunkTarget} and next target point {e.analogTarget}");
     }
 
     private void OnTriggerEnter(Collider trigger)
@@ -93,24 +168,8 @@ public class PlayerController : MonoBehaviour
 
         if (trigger.transform.parent.TryGetComponent(out ChunkHandler chunk))
         {
-            EnableNavMeshCharacterControl();
             GameManager.Instance.EnteredTheChunk(chunk);
         }
     }
 
-    private void EnableAnalogCharacterControl()
-    {
-
-        Debug.Log($"PLAYER: NavMesh Agent was disabled");
-        _analogCharacterControlEnabled = true;
-        _agent.enabled = false;
-    }
-
-    private void EnableNavMeshCharacterControl()
-    {
-
-        Debug.Log($"PLAYER: NavMesh Agent was enabled");
-        _analogCharacterControlEnabled = false;
-        _agent.enabled = true;
-    }
 }
